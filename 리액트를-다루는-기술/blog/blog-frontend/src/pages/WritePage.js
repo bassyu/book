84361b786f429c754e153011c25aa0b6f020a504
bassyu <yrkdgus@gmail.com>
2022-09-06 21:23:31 +0900

    remove .git from blog/blog-frontend

---
+++ 리액트를-다루는-기술/blog/blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
+
+const WritePage = () => {
+  return (
+    <AuthTemplate>
+      <AuthForm />
+    </AuthTemplate>
+  );
+};
+
+export default WritePage;
